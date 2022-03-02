88789094ea690a7441fc0fc4cd0984a40b674c44
Matej Šopor <60981871+MatejSopor@users.noreply.github.com>
2022-03-02 12:24:48 +0100

    Add files via upload

---
+++ filter/src/App.js
@@ -0,0 +1,35 @@
+
+import React, { useState } from 'react';
+import Menu from './components/Menu';
+import Categories from './components/Categories';
+import items from './data';
+const allCategories = ['Všetky', ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'Všetky') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Ponuka</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ filter/src/data.js
@@ -0,0 +1,85 @@
+import Img1 from './Img/nikon-d7200.png'
+import Img2 from './Img/nikon_d750.png'
+import Img3 from './Img/nikon_d850.png'
+import Img4 from './Img/canon_5d_3.jpg'
+import Img5 from './Img/canon_5d_4.jpg'
+import Img6 from './Img/canon_7d_2.jpg'
+import Img7 from './Img/sony_a7_2.jpg'
+import Img8 from './Img/sony_a7s_2.jpg'
+import Img9 from './Img/sony_a7r_IV.jpg'
+
+const Ponuka = [
+  {
+    id: 1,
+    title: 'Nikon D7200',
+    category: 'Nikon',
+    price: 350,
+    img: Img1,
+    desc: `Excepteur ad officia ut proident sit aute Lorem nulla aliqua ad eiusmod proident eiusmod. `,
+  },
+  {
+    id: 2,
+    title: 'Nikon D750',
+    category: 'Nikon',
+    price: 550,
+    img: Img2,
+    desc: `Labore laboris ullamco laborum duis cillum exercitation laboris eu ea excepteur tempor officia et. `,
+  },
+  {
+    id: 3,
+    title: 'Nikon D850',
+    category: 'Nikon',
+    price: 800,
+    img: Img3,
+    desc: `Sint duis aliquip ea cupidatat consequat esse est..`,
+  },
+  {
+    id: 4,
+    title: 'Canon EOS 5D MARK III ',
+    category: 'Canon',
+    price: 750,
+    img: Img4,
+    desc: `Lorem cillum ad esse sunt ut id consectetur nisi voluptate ut ex minim., `,
+  },
+  {
+    id: 5,
+    title: 'Canon EOS 5D MARK IV',
+    category: 'Canon',
+    price: 1300,
+    img: Img5,
+    desc: `Commodo do amet nulla irure dolore velit dolore mollit non eiusmod eiusmod ullamco ex laborum. `,
+  },
+  {
+    id: 6,
+    title: 'Canon EOS 7D MARK II',
+    category: 'Canon',
+    price: 650,
+    img: Img6,
+    desc: `Occaecat amet exercitation reprehenderit non in laborum est adipisicing laboris laboris quis eu occaecat.`,
+  },
+  {
+    id: 7,
+    title: 'Sony a7 II',
+    category: 'Sony',
+    price: 820,
+    img: Img7,
+    desc: `Irure est do proident officia magna ullamco exercitation exercitation est. `,
+  },
+  {
+    id: 8,
+    title: 'Sony a7s II',
+    category: 'Sony',
+    price: 950,
+    img: Img8,
+    desc: `Ipsum reprehenderit aliqua est nulla est amet culpa velit sunt ea nulla pariatur fugiat nulla.  `,
+  },
+  {
+    id: 9,
+    title: 'Sony a7R IV',
+    category: 'Sony',
+    price: 2500,
+    img: Img9,
+    desc: `Deserunt pariatur est esse id laborum laboris in mollit dolore adipisicing ea dolore tempor proident.`,
+  },
+];
+export default Ponuka;
